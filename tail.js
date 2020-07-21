912254363fe26c97eb5951bfd8651b8964139334
Serah Sirohi <serahsirohi@gmail.com>
2020-07-21 03:23:11 +0000

    adding implement tail file

---
+++ tail.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if ((actual) === (expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+};
+
+
+const tail = function(arr) {
+  return arr.slice(1);
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+
