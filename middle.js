0c2f724970aff5c8612f4a48fb5b0baaa7b1b2ee
Serah Sirohi <serahsirohi@gmail.com>
2020-07-22 20:25:29 +0000

    adding middle file

---
+++ middle.js
@@ -0,0 +1,52 @@
+const eqArrays = function (actual,expected){
+  if (expected.length===actual.length){  
+    for (i=0; i<actual.length; i++){
+      if (actual[i] !== expected[i]) {
+        return false;
+      } 
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual= function (actual,expected){
+ if (eqArrays(actual, expected)){
+   console.log("true");
+ } else {
+   console.log("false");
+ }
+}
+
+//Implement middle function
+
+let middle = function(orgArray){
+  
+  //it should take in an array and return the middle most elements of the given array
+  //the middle function should return only the middle element(s) of the array which will have varying length. 
+    
+      if ((orgArray.length===1) || (orgArray.length===2)){
+        return [];
+      } else if (orgArray.length % 2 === 0){
+        const secondIndex = Math.floor(orgArray.length/2);
+        const firstIndex = secondIndex - 1;
+        return [orgArray[firstIndex],orgArray[secondIndex]];
+      } else {
+        const midIndex = Math.floor(orgArray.length/2);
+        // console.log[orgArray[midIndex]];
+        return [orgArray[midIndex]];
+      }
+    }
+
+//use the assertArraysEqual to test our new function
+
+//TEST CODE
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
+
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])) // => [3]
+
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
