997c90d82fe1729cdd37182457957dfaa3831956
Serah Sirohi <serahsirohi@gmail.com>
2020-07-27 17:22:07 -0700

    adding assertEqualTest file

---
+++ test/assertEqualTest.js
@@ -0,0 +1,10 @@
+// const assert = require('chai').assert;
+const assertEqual = require('../assertEqual');
+
+//TEST CODE
+assertEqual("\"Lighthouse Labs\"", "\"Bootcamp\"");
+assertEqual(1,1);
+assertEqual(3,3);
+assertEqual(1,"abc");
+assertEqual("abc","abc");
+assertEqual(1,5);
