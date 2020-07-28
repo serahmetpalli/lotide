0faf37f474be7b9e4b74d0aecfa09a7c4f5bd2df
Serah Sirohi <serahsirohi@gmail.com>
2020-07-27 17:11:20 -0700

    adding refactored tailTest file

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,19 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 const result = ["Hello", "Lighthouse", "Labs"];
 
-assertEqual(tail(result).length, 2); // ensure we get back two elements
-assertEqual(tail(result)[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(tail(result)[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  it("returns 2 for [Hello, Lighthouse, Labs].length", () => {
+    assert.strictEqual(tail(result).length, 2);
+  });
+
+  it("returns Lighthouse for [0]", () => {
+    assert.strictEqual(tail(result)[0],"Lighthouse"); 
+  });
+
+  it("returns Labs for [1]", () => {
+    assert.strictEqual(tail(result)[1],"Labs"); 
+  });
+});
 
 
